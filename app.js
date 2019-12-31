8a9f3f5fca28040ede25f74915a9b8c2659663fc
Phạm Văn Điệp <diep.job@outlook.com>
2019-12-31 09:35:42 +0700

    update app.js

--- app.js
+++ app.js
@@ -5,36 +5,21 @@ const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
 
-const productsRouter = require('./routes/products');
-const usersRouter = require('./routes/users');
-const addressesRouter = require('./routes/addresses');
-const commentsRouter = require('./routes/comments');
-const tagAndCategoriesRouter = require('./routes/tags_categories');
-const pagesRouter = require('./routes/pages');
-
-const ordersRouter = require('./routes/orders');
-
-const AuthMiddleware = require('./middlewares/auth');
-const BenchmarkMiddleware = require('./middlewares/benchmark');
-const AppResponseDto = require('./dtos/responses/app_response.dto');
-
 const app = express();
 
-app.use(BenchmarkMiddleware.benchmark);
+app.use(require('./middlewares/benchmark').benchmark);
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(AuthMiddleware.loadUser);
-app.use('/api', productsRouter);
-app.use('/api', usersRouter);
-app.use('/api', commentsRouter);
-app.use('/api', addressesRouter);
-app.use('/api', ordersRouter);
-app.use('/api', tagAndCategoriesRouter);
-app.use('/api', pagesRouter);
+app.use(require('./middlewares/auth').loadUser);
+
+const arrRoutes = ['products', 'users', 'addresses', 'comments', 'tags_categories', 'pages', 'orders'];
+arrRoutes.forEach( function(route) {
+  app.use('/api', require('./routes/' + route) );
+} );
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -45,7 +30,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.json(AppResponseDto.buildWithErrorMessages(err));
+  res.json( require('./dtos/responses/app_response').buildWithErrorMessages(err));
 });
 
 module.exports = app;
